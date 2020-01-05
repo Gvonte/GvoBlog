85498579db014c601bdb1a588806821439335dfa
Gvonte <1158896795@qq.com>
2020-01-05 16:37:31 +0800

    GvoBlog 1.0.0

---
+++ public/precache-manifest.e65c7e5c366bc1574aa63b99da35fb3f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a4027ecdbd2221526282e4a020764d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "434c507b458bb520b802",
+    "url": "/static/css/2.93de4abf.chunk.css"
+  },
+  {
+    "revision": "81909998b49259483cfb",
+    "url": "/static/css/main.c307ac49.chunk.css"
+  },
+  {
+    "revision": "434c507b458bb520b802",
+    "url": "/static/js/2.9bd45cc5.chunk.js"
+  },
+  {
+    "revision": "e2e2e4e52bd2feb89204681266bd7fa7",
+    "url": "/static/js/2.9bd45cc5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "81909998b49259483cfb",
+    "url": "/static/js/main.b05fc7a2.chunk.js"
+  },
+  {
+    "revision": "919ac4a14c89f4e9301e",
+    "url": "/static/js/runtime-main.ddb9ed25.js"
+  },
+  {
+    "revision": "db29b0fbd2f78dd8c1b7f79413dd7581",
+    "url": "/static/media/back.db29b0fb.png"
+  },
+  {
+    "revision": "1427c1697492583a48285224c3f6cd52",
+    "url": "/static/media/brand.1427c169.png"
+  }
+]);
