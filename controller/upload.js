5f3a30fd16f0e3a72a15bf8acce9ece90d8a639b
Gvonte <1158896795@qq.com>
2020-01-09 22:58:58 +0800

    GvoBlog v1.1.1

--- controller/upload.js
+++ controller/upload.js
@@ -1,7 +1,18 @@
+const path = require("path");
+const fs = require("fs");
 // 上传一个文件
 const upload = async ctx => {
     // console.log(ctx.req.file); // 注意数据存储在原始请求中
     // console.log(ctx.req.body); // 注意数据存储在原始请求中
+
+    const file = ctx.req.file;
+    // 如果是图片，需要将图片的二进制文件存一份img格式的在/root/img目录下
+    if (file.mimetype.indexOf('image') > -1) {
+        fs.readFile(path.resolve(file.path), (err, data) => {
+            fs.writeFileSync(`/root/img/${file.filename}.${file.originalname.split('.')[1]}`, data)
+        })
+    }
+
     ctx.response.status = 200;
     ctx.body = global.res.json({ file: ctx.req.file, body: ctx.req.body });
 }
--- routes/upload.js
+++ routes/upload.js
@@ -5,7 +5,7 @@ const path = require("path");
 const jwtAuth = require("koa-jwt");
 const secret = require("../utils/tokenSecret");
 const upload1 = require("koa-multer")({ dest: "./public/doc" });
-const upload2 = require("koa-multer")({ dest: "/root/img" });
+const upload2 = require("koa-multer")({ dest: "./public/img" });
 
 router.post("/upload", jwtAuth({ secret }), upload1.single("file"), uploadController.upload); // 上传一个文件
 router.post("/uploadImg", jwtAuth({ secret }), upload2.single("file"), uploadController.upload); // 上传一张图片
